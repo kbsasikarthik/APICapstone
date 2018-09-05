6dc3d835b36569f06318123518ce510266c84fd3
Sasi K <kbsasikarthik@gmail.com>
2018-09-05 09:38:06 -0400

    sturcture created

---
+++ src/main/java/co/grandcircus/APICapstone/ApiCapstoneApplication.java
@@ -0,0 +1,12 @@
+package co.grandcircus.APICapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCapstoneApplication.class, args);
+	}
+}
---
+++ src/test/java/co/grandcircus/APICapstone/ApiCapstoneApplicationTests.java
@@ -0,0 +1,16 @@
+package co.grandcircus.APICapstone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiCapstoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
